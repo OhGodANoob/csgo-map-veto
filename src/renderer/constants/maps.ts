d99184075fe78c9da987f4f61d4ab838bcf6178a
OhGodANoob <46369378+OhGodANoob@users.noreply.github.com>
2023-05-15 04:45:50 -0400

    Update maps.ts
    Update map pool to current active duty maps

--- src/renderer/constants/maps.ts
+++ src/renderer/constants/maps.ts
@@ -1,11 +1,9 @@
 export const MAPS = [
-  'de_dust2',
+  'de_anubis',
+  'de_ancient',
   'de_inferno',
-  'de_cache',
-  'de_cobblestone',
   'de_mirage',
   'de_overpass',
   'de_nuke',
-  'de_train',
   'de_vertigo',
 ];
